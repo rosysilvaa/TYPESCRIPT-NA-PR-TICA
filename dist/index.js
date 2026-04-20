7ffa7a51eed1841cf4af4de932e46a9ce4dd73f2
Roseane da Silva <158008614+rosysilvaa@users.noreply.github.com>
2026-04-20 16:27:08 -0300

    Add files via upload

---
+++ dist/index.js
@@ -0,0 +1,57 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importStar(require("express"));
+const app = (0, express_1.default)();
+let products = IProducts[] = [];
+app.get('/produtos', (req, res) => {
+    const newProduct = {
+        id: Math.random(),
+        name: "Batedeira",
+        price: 300,
+        description: "Batedeira, muito boa , boa mesmo",
+        address: {
+            street: "Rua dos Bobos",
+            number: 0,
+            state: "SP"
+        }
+    };
+    products.push(newProduct);
+    return res.json(products);
+});
+app.listen(3000, () => {
+    console.log('Servidor rodando');
+});
+//# sourceMappingURL=index.js.map
